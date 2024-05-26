11ebae670560b58d1524849c4c9266fc6453ce4b
DIOSDEDIOS <moneypares@gmail.com>
2024-05-26 02:07:07 -0600

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get("/", (req, res) => {
+    //root route http://localhost:5000
+//    res.send("¡HOLA MUNDO!");
+// });
+
+
+app.listen(PORT,() => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`);
+});
